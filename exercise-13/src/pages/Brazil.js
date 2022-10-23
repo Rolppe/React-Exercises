6bbf49460676efe6d9ad61d54ae5815c04dadf57
Mikko Paaso <mikko@Mikko-MacBook-Pro.local>
2022-10-23 14:19:10 +0300

    exercise-13 done

--- exercise-13/src/pages/Brazil.js
+++ exercise-13/src/pages/Brazil.js
@@ -1,5 +1,38 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import Modal from "../components/Modal";
+import Backdrop from "../components/Backdrop";
+
 const BrazilPage = () => {
-  return <h2>Welcome to beautiful (and tasty!) Brazil!</h2>;
+  const [showModal, setShowModal] = useState(false);
+  const history = useHistory();
+
+  const showModalHandler = () => {
+    setShowModal(true);
+  };
+
+  const cancelModalHandler = () => {
+    setShowModal(false);
+  };
+
+  const randomHandler = () => {
+    history.push(Math.random() > 0.5 ? "./" : "./italy");
+  };
+
+  const backToFinlandHandler = () => {
+    history.push("/");
+  };
+  return (
+    <div>
+      <h2>Welcome to beautiful (and tasty!) Brazil!</h2>
+      <button onClick={backToFinlandHandler}>Back to finland</button>
+      <button onClick={showModalHandler}>Go to random destination</button>
+      {showModal && (
+        <Modal onCancel={cancelModalHandler} onConfirm={randomHandler} />
+      )}
+      {showModal ? <Backdrop onClick={cancelModalHandler} /> : null}
+    </div>
+  );
 };
 
 export default BrazilPage;
--- exercise-13/src/pages/Italy.js
+++ exercise-13/src/pages/Italy.js
@@ -1,5 +1,39 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import Modal from "../components/Modal";
+import Backdrop from "../components/Backdrop";
+
 const ItalyPage = () => {
-  return <h2>Welcome to tasty (and beautiful!) Italy!</h2>;
+  const [showModal, setShowModal] = useState(false);
+  const history = useHistory();
+
+  const showModalHandler = () => {
+    setShowModal(true);
+  };
+
+  const cancelModalHandler = () => {
+    setShowModal(false);
+  };
+
+  const randomHandler = () => {
+    history.push(Math.random() > 0.5 ? "./" : "./brazil");
+  };
+
+  const backToFinlandHandler = () => {
+    history.push("/");
+  };
+
+  return (
+    <div>
+      <h2>Welcome to tasty (and beautiful!) Italy!</h2>
+      <button onClick={backToFinlandHandler}>Back to finland</button>
+      <button onClick={showModalHandler}>Go to random destination</button>
+      {showModal && (
+        <Modal onCancel={cancelModalHandler} onConfirm={randomHandler} />
+      )}
+      {showModal ? <Backdrop onClick={cancelModalHandler} /> : null}
+    </div>
+  );
 };
 
 export default ItalyPage;
