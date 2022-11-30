4b54efe698668fb19fef2233224d717b33c9a2cf
Mikko Paaso <mikko@Mikko-MacBook-Pro.local>
2022-11-30 13:15:50 +0200

    Basic data > PriceList > PriceItem done

--- project_exercise/src/components/Functions.js
+++ project_exercise/src/components/Functions.js
@@ -1,18 +1,3 @@
-export async function fetchPrices() {
-  let priceArray = [];
-  const response = await fetch(
-    "https://ohjelmistoprojekti-production.up.railway.app/pricejson/"
-  );
-  const data = await response.json();
-  let [dateToday, dateTomorrow] = getDateTodayAndTomorrow();
-  let pricesToday = getCurrentPrices(dateToday, data);
-  let pricesTomorrow = getCurrentPrices(dateTomorrow, data);
-  return [pricesToday, pricesTomorrow];
-  // for (let unit in data.Prices[3]) {
-  //   console.log(unit);
-  //   //priceArray.push()
-  // }
-}
 export function getDateTodayAndTomorrow() {
   var today = new Date();
   var tomorrow = new Date();
@@ -35,32 +20,32 @@ export function getCurrentPrices(currentDate, data) {
       //console.log("getCurrentPrices date: " + currentDate);
       //console.log(data.Prices[i].Date);
 
-      const currentPrices = {
-        Klo_00: data.Prices[i].H00,
-        Klo_01: data.Prices[i].H01,
-        Klo_02: data.Prices[i].H02,
-        Klo_03: data.Prices[i].H03,
-        Klo_04: data.Prices[i].H04,
-        Klo_05: data.Prices[i].H05,
-        Klo_06: data.Prices[i].H06,
-        Klo_07: data.Prices[i].H07,
-        Klo_08: data.Prices[i].H08,
-        Klo_09: data.Prices[i].H09,
-        Klo_10: data.Prices[i].H10,
-        Klo_11: data.Prices[i].H11,
-        Klo_12: data.Prices[i].H12,
-        Klo_13: data.Prices[i].H13,
-        Klo_14: data.Prices[i].H14,
-        Klo_15: data.Prices[i].H15,
-        Klo_16: data.Prices[i].H16,
-        Klo_17: data.Prices[i].H17,
-        Klo_18: data.Prices[i].H18,
-        Klo_19: data.Prices[i].H19,
-        Klo_20: data.Prices[i].H20,
-        Klo_21: data.Prices[i].H21,
-        Klo_22: data.Prices[i].H22,
-        Klo_23: data.Prices[i].H23,
-      };
+      const currentPrices = [
+        data.Prices[i].H00,
+        data.Prices[i].H01,
+        data.Prices[i].H02,
+        data.Prices[i].H03,
+        data.Prices[i].H04,
+        data.Prices[i].H05,
+        data.Prices[i].H06,
+        data.Prices[i].H07,
+        data.Prices[i].H08,
+        data.Prices[i].H09,
+        data.Prices[i].H10,
+        data.Prices[i].H11,
+        data.Prices[i].H12,
+        data.Prices[i].H13,
+        data.Prices[i].H14,
+        data.Prices[i].H15,
+        data.Prices[i].H16,
+        data.Prices[i].H17,
+        data.Prices[i].H18,
+        data.Prices[i].H19,
+        data.Prices[i].H20,
+        data.Prices[i].H21,
+        data.Prices[i].H22,
+        data.Prices[i].H23,
+      ];
       return currentPrices;
     }
   }
--- project_exercise/src/pages/Home.js
+++ project_exercise/src/pages/Home.js
@@ -1,36 +1,53 @@
+import { type } from "@testing-library/user-event/dist/type";
 import React from "react";
 import { useEffect, useState } from "react";
 import {
   getDateTodayAndTomorrow,
   getCurrentPrices,
-  fetchData,
 } from "../components/Functions";
+import PriceList from "../components/PriceList";
 
 export default function Home(props) {
+  const [priceData, setpriceData] = useState(null);
   const [todayPrices, setTodayPrices] = useState([]);
+  const [pricesToday, setPricesToday] = useState([null]);
+
+  let [dateToday, dateTomorrow] = getDateTodayAndTomorrow();
+  let pricesTomorrow = null;
 
   useEffect(() => {
-    console.log("useEffect before");
-    let [pricesTodat, pricesTomorrow] = fetc;
-    setTodayPrices(fetchData());
-    console.log("useEffect after");
+    fetch("https://ohjelmistoprojekti-production.up.railway.app/pricejson/")
+      .then((result) => {
+        return result.json();
+      })
+      .then((data) => {
+        //console.log(data.Prices[0].H00);
+        setPricesToday(getCurrentPrices(dateToday, data));
+        //console.log(pricesToday);
+        //setpriceData(data);
+        //console.log(pricesToday);
+      })
+      .then(() => {});
   }, []);
 
   useEffect(() => {
-    console.log("useEffect updated before");
-    console.log("Home useEffect: " + todayPrices);
-    //console.log("Home useEffect: " + todayPrices.Klo_00);
-  }, [todayPrices]);
+    console.log("type of prices today: " + pricesToday[0]);
+  }, [pricesToday]);
 
+  // function extractPricesArray(props) {
+  //   const pricesArray = [];
+  //   //const dummy = props.map((price) => console.log(price));
+  //   return props.map((price) => <div>{price}</div>);
+  // }
+
+  //<div>{prices.Klo_00}</div>
   return (
     <main style={{ padding: "1rem 0" }}>
       <h2>Home</h2>
-      <ul>
-        <h3>Prices today</h3>
-        <div>klo 00:00 price = </div>
-      </ul>
+      <ul>{pricesToday && <PriceList pricesToday={pricesToday} />}</ul>
     </main>
   );
 }
 
-//klo: {time}.00 price: {todayPrices[time]}/kWh
+//klo: {time}.00 price: {todayPrices[time]}
+//<div>{pricesToday && extractPricesArray(pricesToday)}</div>
