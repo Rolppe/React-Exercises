0c1b890c4125ea299667aedcc41efb1508b1f74b
Mikko Paaso <mikko@Mikko-MacBook-Pro.local>
2022-12-18 13:57:59 +0200

    commented all out of app

--- project_exercise/src/App.js
+++ project_exercise/src/App.js
@@ -5,9 +5,6 @@ import HappyHour from "./pages/HappyHour";
 import Settings from "./pages/Settings";
 import Prices from "./pages/Prices";
 import Layout from "./components/Layout";
-
-import { initializeApp } from "firebase/app";
-
 import {
   getDateTodayAndTomorrow,
   getCurrentPrices,
@@ -16,98 +13,99 @@ import {
 } from "./components/Functions";
 
 function App() {
-  //  content_copy;
+  // //  content_copy;
 
-  const [twoHoursProgram, setTwoHoursProgram] = useState(false);
-  const [threeHoursProgram, setThreeHoursProgram] = useState(false);
-  const [fourHoursProgram, setFourHoursProgram] = useState(false);
-  const [fiveHoursProgram, setFiveHoursProgram] = useState(false);
-  const [dateToday, dateTomorrow] = getDateTodayAndTomorrow();
-  const [pricesToday, setPricesToday] = useState([null]);
-  const [pricesTomorrow, setPricesTomorrow] = useState([null]);
-  const [settingsArrived, setSettingsArrived] = useState(false);
+  // const [twoHoursProgram, setTwoHoursProgram] = useState(false);
+  // const [threeHoursProgram, setThreeHoursProgram] = useState(false);
+  // const [fourHoursProgram, setFourHoursProgram] = useState(false);
+  // const [fiveHoursProgram, setFiveHoursProgram] = useState(false);
+  // const [dateToday, dateTomorrow] = getDateTodayAndTomorrow();
+  // const [pricesToday, setPricesToday] = useState([null]);
+  // const [pricesTomorrow, setPricesTomorrow] = useState([null]);
+  // const [settingsArrived, setSettingsArrived] = useState(false);
 
-  // initial start
-  useEffect(() => {
-    // get saved settings
-    getSettings(
-      setTwoHoursProgram,
-      setThreeHoursProgram,
-      setFourHoursProgram,
-      setFiveHoursProgram,
-      setSettingsArrived
-    );
+  // // initial start
+  // useEffect(() => {
+  //   // get saved settings
+  //   getSettings(
+  //     setTwoHoursProgram,
+  //     setThreeHoursProgram,
+  //     setFourHoursProgram,
+  //     setFiveHoursProgram,
+  //     setSettingsArrived
+  //   );
 
-    // Fetch price data from server and then set it to useStates
-    fetch("https://ohjelmistoprojekti-production.up.railway.app/pricejson/")
-      .then((result) => {
-        return result.json();
-      })
-      .then((data) => {
-        setPricesToday(getCurrentPrices(dateToday, data));
-        setPricesTomorrow(getCurrentPrices(dateTomorrow, data));
-        setPricesToday(getCurrentPrices(dateToday, data));
-      });
-  }, []);
+  //   // Fetch price data from server and then set it to useStates
+  //   fetch("https://ohjelmistoprojekti-production.up.railway.app/pricejson/")
+  //     .then((result) => {
+  //       return result.json();
+  //     })
+  //     .then((data) => {
+  //       setPricesToday(getCurrentPrices(dateToday, data));
+  //       setPricesTomorrow(getCurrentPrices(dateTomorrow, data));
+  //       setPricesToday(getCurrentPrices(dateToday, data));
+  //     });
+  // }, []);
 
-  // if settings chances store them
-  useEffect(() => {
-    if (settingsArrived) {
-      storeSettings(
-        twoHoursProgram,
-        threeHoursProgram,
-        fourHoursProgram,
-        fiveHoursProgram
-      );
-    }
-  }, [twoHoursProgram, threeHoursProgram, fourHoursProgram, fiveHoursProgram]);
+  // // if settings chances store them
+  // useEffect(() => {
+  //   if (settingsArrived) {
+  //     storeSettings(
+  //       twoHoursProgram,
+  //       threeHoursProgram,
+  //       fourHoursProgram,
+  //       fiveHoursProgram
+  //     );
+  //   }
+  // }, [twoHoursProgram, threeHoursProgram, fourHoursProgram, fiveHoursProgram]);
 
   return (
-    <div className="Main">
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route
-            index
-            element={
-              <Prices
-                pricesToday={pricesToday}
-                pricesTomorrow={pricesTomorrow}
-              />
-            }
-          />
-          <Route
-            path="HappyHour"
-            element={
-              <HappyHour
-                twoHoursProgram={twoHoursProgram}
-                threeHoursProgram={threeHoursProgram}
-                fourHoursProgram={fourHoursProgram}
-                fiveHoursProgram={fiveHoursProgram}
-                pricesToday={pricesToday}
-                pricesTomorrow={pricesTomorrow}
-                setTwoHoursProgram={setTwoHoursProgram}
-                setThreeHoursProgram={setThreeHoursProgram}
-              />
-            }
-          />
-          <Route
-            path="Settings"
-            element={
-              <Settings
-                twoHoursProgram={twoHoursProgram}
-                threeHoursProgram={threeHoursProgram}
-                fourHoursProgram={fourHoursProgram}
-                fiveHoursProgram={fiveHoursProgram}
-                setTwoHoursProgram={setTwoHoursProgram}
-                setThreeHoursProgram={setThreeHoursProgram}
-                setFourHoursProgram={setFourHoursProgram}
-                setFiveHoursProgram={setFiveHoursProgram}
-              />
-            }
-          />
-        </Route>
-      </Routes>
-    </div>
+    <div></div>
+    // <div className="Main">
+    //   <Routes>
+    //     <Route path="/" element={<Layout />}>
+    //       <Route
+    //         index
+    //         element={
+    //           <Prices
+    //             pricesToday={pricesToday}
+    //             pricesTomorrow={pricesTomorrow}
+    //           />
+    //         }
+    //       />
+    //       <Route
+    //         path="HappyHour"
+    //         element={
+    //           <HappyHour
+    //             twoHoursProgram={twoHoursProgram}
+    //             threeHoursProgram={threeHoursProgram}
+    //             fourHoursProgram={fourHoursProgram}
+    //             fiveHoursProgram={fiveHoursProgram}
+    //             pricesToday={pricesToday}
+    //             pricesTomorrow={pricesTomorrow}
+    //             setTwoHoursProgram={setTwoHoursProgram}
+    //             setThreeHoursProgram={setThreeHoursProgram}
+    //           />
+    //         }
+    //       />
+    //       <Route
+    //         path="Settings"
+    //         element={
+    //           <Settings
+    //             twoHoursProgram={twoHoursProgram}
+    //             threeHoursProgram={threeHoursProgram}
+    //             fourHoursProgram={fourHoursProgram}
+    //             fiveHoursProgram={fiveHoursProgram}
+    //             setTwoHoursProgram={setTwoHoursProgram}
+    //             setThreeHoursProgram={setThreeHoursProgram}
+    //             setFourHoursProgram={setFourHoursProgram}
+    //             setFiveHoursProgram={setFiveHoursProgram}
+    //           />
+    //         }
+    //       />
+    //     </Route>
+    //   </Routes>
+    // </div>
   );
 }
 
